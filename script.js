db2bd5605e872c62ba93dcd3d2f6153ddfd6e726
Feliclare De Jesus <feliclare@Feliclares-MacBook-Pro.local>
2023-01-08 11:47:41 -0800

    reorganize javascript

--- script.js
+++ script.js
@@ -1,57 +1,58 @@
-//time block variables per hour
-//09:00
+//time variables
 var nineEl = $('#hour-9');
-var hourNine= dayjs().hour(9).minute(00).format('HH:mm')
-var setHourNine= nineEl.children('div')
-setHourNine.text(hourNine);
-
-//10:00
 var tenEl = $('#hour-10');
-var hourTen= dayjs().hour(10).minute(00).format('HH:mm')
-var setHourTen= tenEl.children('div')
-setHourTen.text(hourTen);
-
-//11:00
 var elevenEl = $('#hour-11');
-var hourEleven= dayjs().hour(11).minute(00).format('HH:mm')
-var setHourEleven= elevenEl.children('div')
-setHourEleven.text(hourEleven);
-
-//12:00
 var twelveEl = $('#hour-12');
-var hourTwelve= dayjs().hour(12).minute(00).format('HH:mm')
-var setHourTwelve= twelveEl.children('div')
-setHourTwelve.text(hourTwelve);
-
-//13:00
 var thirteenEl = $('#hour-13');
-var hourThirteen= dayjs().hour(13).minute(00).format('HH:mm')
-var setHourThirteen= thirteenEl.children('div')
-setHourThirteen.text(hourThirteen);
-
-//14:00
 var fourteenEl = $('#hour-14');
-var hourFourteen= dayjs().hour(14).minute(00).format('HH:mm')
-var setHourFourteen= fourteenEl.children('div')
-setHourFourteen.text(hourFourteen);
-
-//15:00
 var fifteenEl = $('#hour-15');
-var hourFifteen= dayjs().hour(15).minute(00).format('HH:mm')
-var setHourFifteen= fifteenEl.children('div')
-setHourFifteen.text(hourFifteen);
-
-//16:00
 var sixteenEl = $('#hour-16');
-var hourSixteen= dayjs().hour(16).minute(00).format('HH:mm')
-var setHourSixteen= sixteenEl.children('div')
-setHourSixteen.text(hourSixteen);
-
-//17:00
 var seventeenEl = $('#hour-17');
+
+//button variables
+var saveBtn9= $(".saveBtn9")
+var saveBtn10= $(".saveBtn10")
+var saveBtn11= $(".saveBtn11")
+var saveBtn12= $(".saveBtn12")
+var saveBtn13= $(".saveBtn13")
+var saveBtn14= $(".saveBtn14")
+var saveBtn15= $(".saveBtn15")
+var saveBtn16= $(".saveBtn16")
+var saveBtn17= $(".saveBtn17")
+
+//dayjs for hour blocks
+var hourNine= dayjs().hour(9).minute(00).format('HH:mm')
+var hourTen= dayjs().hour(10).minute(00).format('HH:mm')
+var hourEleven= dayjs().hour(11).minute(00).format('HH:mm')
+var hourTwelve= dayjs().hour(12).minute(00).format('HH:mm')
+var hourThirteen= dayjs().hour(13).minute(00).format('HH:mm')
+var hourFourteen= dayjs().hour(14).minute(00).format('HH:mm')
+var hourFifteen= dayjs().hour(15).minute(00).format('HH:mm')
+var hourSixteen= dayjs().hour(16).minute(00).format('HH:mm')
 var hourSeventeen= dayjs().hour(17).minute(00).format('HH:mm')
-var setHourSeventeen= seventeenEl.children('div')
-setHourSeventeen.text(hourSeventeen);
+
+//display dayjs in the hour block div
+nineEl.children('div').text(hourNine);
+tenEl.children('div').text(hourTen);
+elevenEl.children('div').text(hourEleven);
+twelveEl.children('div').text(hourTwelve);
+thirteenEl.children('div').text(hourThirteen);
+fourteenEl.children('div').text(hourFourteen);
+fifteenEl.children('div').text(hourFifteen);
+sixteenEl.children('div').text(hourSixteen);
+seventeenEl.children('div').text(hourSeventeen);
+
+// setInterval to display current date and time in header
+setInterval(function(){
+  var dateTime= dayjs().format('MMMM D, YYYY [at] HH:mm:ss');
+  $('#currentDay').text(dateTime);
+}, 1000);
+
+//saveInput function localStorage
+
+
+//event listenter for save button
+saveBtn.addEventListener("click",saveInput);
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
@@ -63,12 +64,3 @@ $(function () {
 
  
 });
-
- // Function to display current date and time in header
- function seconds(){
-  var currentDate= dayjs().format('MMMM D, YYYY');
-  var currentTime= dayjs().format('HH:mm:ss');
-  $('#currentDay').text(currentDate + ' at ' + currentTime);
-}
-//setInterval to refresh function every second
-setInterval(seconds,1000);
